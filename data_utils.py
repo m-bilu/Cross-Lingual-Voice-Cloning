data_utils normalization fix

data_utils.py
@@ -43,7 +43,7 @@ class TextMelLoader(torch.utils.data.Dataset):
             if sampling_rate != self.stft.sampling_rate:
                 raise ValueError("{} SR doesn't match target {} SR".format(
                     sampling_rate, self.stft.sampling_rate))
-            audio_norm = audio / self.max_wav_value
+            audio_norm = audio / max(abs(torch.min(audio.data)), abs(torch.max(audio.data)))
             audio_norm = audio_norm.unsqueeze(0)
             audio_norm = torch.autograd.Variable(audio_norm, requires_grad=False)
             melspec = self.stft.mel_spectrogram(audio_norm)